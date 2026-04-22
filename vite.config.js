dfb8921ac040297ab57f24927eaf5d74e64ca462
HaoLam <haolam@HaoLamdeMacBook-Air.local>
2026-04-22 11:53:30 +0800

    Add OCR scanning workflow

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: './'
+  base: './',
+  server: {
+    proxy: {
+      '/ocr-api': {
+        target: 'http://192.168.4.68:8000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/ocr-api/, '')
+      }
+    }
+  }
 })
